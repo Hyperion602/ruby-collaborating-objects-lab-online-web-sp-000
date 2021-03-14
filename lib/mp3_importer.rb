650fd878e3864ce58aebad9ca4dc9b5b62f10e2d
James Harris <mharris9608@yahoo.com>
2021-03-14 17:27:48 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,4 +1,4 @@
-class MP3Importer
+Class MP3Importer
   attr_accessor :path
 
   def initialize(path)
@@ -6,16 +6,10 @@ class MP3Importer
   end
 
   def files
-    files = []
-    Dir.new(self.path).each do |file|
-      files << file if file.length > 4
-    end
-    files
+    Dir[@path+"/*.mp3"].map { |file| file.split("/").last }
   end
 
   def import
-    self.files.each do |filename|
-      Song.new_by_filename(filename)
-    end
+    files.each { |file| Song.new_by_filename(file) }
   end
 end
